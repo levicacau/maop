8910ae9ad9f94adec7fd6405ca9e17534a87cbe2
Levi Cacau <levicacau@gmail.com>
2022-07-11 14:23:01 -0500

    Ajustes para execução dos assetments no path

--- src/assessment/metrics/juntar_tudo_tudo.sh
+++ src/assessment/metrics/juntar_tudo_tudo.sh
@@ -1,9 +1,10 @@
  problems="dtlz1 dtlz2 dtlz3 dtlz4 dtlz5 dtlz6 dtlz7 wfg1 wfg2 wfg3 wfg4 wfg5 wfg6 wfg7 wfg8 wfg9"
  problems=${problems^^}
- for problems in dtlz1 dtlz2 dtlz3 dtlz4 dtlz5 dtlz6 dtlz7 wfg1 wfg2 wfg3 wfg4 wfg5 wfg6 wfg7 wfg8 wfg9; do
+ basePath=./src/assessment/metrics
+ for problem in $problems; do
 #for problem in dtlz1; do
 	#./assessment/metrics/juntar_tudo_gd.sh $problem &
   #./juntar_tudo_igd.sh $problem &
 	#./assessment/metrics/juntar_tudo_r2.sh $problem &
- 	./juntar_tudo_hv.sh $problem &
+	$basePath/juntar_tudo_hv.sh $problem &
 done
